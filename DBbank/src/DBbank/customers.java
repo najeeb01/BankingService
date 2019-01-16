57ac5343255d3b79c04875ebf8de9c580b29c509
Najeeb <najeebs89@gmail.com>
2019-01-16 00:36:53 -0600

    First DBbank commit

---
+++ DBbank/src/DBbank/customers.java
@@ -0,0 +1,67 @@
+package DBbank;
+
+import java.sql.*;
+
+public class customers {
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/dbbank";
+
+   //  Database credentials
+   static final String USER = "root";
+   static final String PASS = "root";
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      //STEP 2: Register JDBC driver
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to a selected database...");
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      System.out.println("Connected database successfully...");
+      
+      //STEP 4: Execute a query
+      System.out.println("Creating table in given database...");
+      stmt = conn.createStatement();
+      
+      
+      String sql = "SELECT * FROM dbbank.customers";
+      stmt = conn.createStatement();
+      
+      ResultSet rs = stmt.executeQuery(sql);	
+      while(rs.next()){
+    	  String id = rs.getString(1);
+    	  String f_name = rs.getString(2);
+    	  String l_name = rs.getString(3);
+    	  String vs = rs.getString(4);
+    	  System.out.print(id + f_name + vs);
+    	  
+			}
+      
+      System.out.println("Created table in given database...");
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      //finally block used to close resources
+      try{
+         if(stmt!=null)
+            conn.close();
+      }catch(SQLException se){
+      }// do nothing
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+   }//end try
+   System.out.println("Goodbye!");
+}//end main
+}//end JDBC
